0b3ea58be2062ed6c85d5bdc82b8603b93910a1c
aymensellaouti <aymen.sellaouti@gmail.com>
2024-04-29 17:30:26 +0100

    comment old modules

--- src/app/cv/cv.module.ts
+++ src/app/cv/cv.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { AddCvComponent } from "./add-cv/add-cv.component";
-import { CvComponent } from "./cv/cv.component";
-import { DetailCardComponent } from "./detail-card/detail-card.component";
-import { EmbaucheComponent } from "./embauche/embauche.component";
-import { ItemComponent } from "./item/item.component";
-import { ListComponent } from "./list/list.component";
-import { DefaultImagePipe } from "./pipes/default-image.pipe";
-import { DetailsComponent } from "./details/details.component";
-import { CvRoutingModule } from "./cv.routing";
+// import { NgModule } from "@angular/core";
+// import { CommonModule } from "@angular/common";
+// import { FormsModule } from "@angular/forms";
+// import { AddCvComponent } from "./add-cv/add-cv.component";
+// import { CvComponent } from "./cv/cv.component";
+// import { DetailCardComponent } from "./detail-card/detail-card.component";
+// import { EmbaucheComponent } from "./embauche/embauche.component";
+// import { ItemComponent } from "./item/item.component";
+// import { ListComponent } from "./list/list.component";
+// import { DefaultImagePipe } from "./pipes/default-image.pipe";
+// import { DetailsComponent } from "./details/details.component";
+// import { CvRoutingModule } from "./cv.routing";
 
-@NgModule({
-    imports: [CommonModule, FormsModule, CvComponent,
-        ItemComponent,
-        ListComponent,
-        DetailCardComponent,
-        DefaultImagePipe,
-        EmbaucheComponent,
-        AddCvComponent,
-        DetailsComponent],
-})
-export class CvModule {}
+// @NgModule({
+//     imports: [CommonModule, FormsModule, CvComponent,
+//         ItemComponent,
+//         ListComponent,
+//         DetailCardComponent,
+//         DefaultImagePipe,
+//         EmbaucheComponent,
+//         AddCvComponent,
+//         DetailsComponent],
+// })
+// export class CvModule {}
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from "@angular/core";
-import { TodoComponent } from "./todo/todo.component";
-import { WeekTodoComponent } from "./week-todo/week-todo.component";
-import { TodoRoutingModule } from "./todo.routing";
-import { FormsModule } from "@angular/forms";
+// import { NgModule } from "@angular/core";
+// import { TodoComponent } from "./todo/todo.component";
+// import { WeekTodoComponent } from "./week-todo/week-todo.component";
+// import { TodoRoutingModule } from "./todo.routing";
+// import { FormsModule } from "@angular/forms";
 
 
-@NgModule({
-    imports: [
-        TodoRoutingModule,
-        FormsModule,
-        TodoComponent, WeekTodoComponent
-    ],
-})
-export class TodoModule {}
+// @NgModule({
+//     imports: [
+//         TodoRoutingModule,
+//         FormsModule,
+//         TodoComponent, WeekTodoComponent
+//     ],
+// })
+// export class TodoModule {}
