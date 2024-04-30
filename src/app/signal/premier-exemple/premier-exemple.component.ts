24ce5e360c765157dc13c9d07a075b50e1a6d293
aymensellaouti <aymen.sellaouti@gmail.com>
2024-04-30 13:50:58 +0100

    delete TodoSignalService

--- src/app/signal/premier-exemple/premier-exemple.component.ts
+++ src/app/signal/premier-exemple/premier-exemple.component.ts
@@ -11,10 +11,21 @@ import { FormsModule } from '@angular/forms';
 export class PremierExempleComponent {
   x = signal(5);
   y = signal(7);
+  firstname = signal('aymen');
+  name = signal('sellaouti');
+  fullname = computed(() => `${this.firstname()} ${this.name()}`);
+  sayHello = computed(() => 'Hello! ' + this.fullname());
 
   xx = 5;
   yy = 7;
   coeff = 5;
   z = computed(() => this.coeff * (this.x() + this.y()));
   zz = computed(() => this.xx + this.yy);
+
+  update() {
+    this.name.update((currentSignalValue) => currentSignalValue + '++');
+  }
+  setSignal() {
+    this.name.set('une Nouvelle valeur définit par set');
+  }
 }
